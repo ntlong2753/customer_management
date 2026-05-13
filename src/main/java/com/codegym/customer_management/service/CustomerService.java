b78726e9c91d10eb8777d123c4fb208a716d5961
ntlong2753 <nguyenthanhlongvn03@gmail.com>
2026-05-13 10:58:36 +0700

    Initial commit

---
+++ src/main/java/com/codegym/customer_management/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.codegym.customer_management.service;
+
+import com.codegym.customer_management.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+    Customer findById(int id);
+}
